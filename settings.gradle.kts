e8dff2e136a214c5e0c004825a81067d8f69be31
RayzalS <160826665+RayzalSelang@users.noreply.github.com>
2024-03-04 19:07:48 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "adv160420103week2-master"
+include(":app")
+ 
